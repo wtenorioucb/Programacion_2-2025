3f44b473e2ef6696ff64b8d27130435a67ed94fa
Willy Tenorio <wtenorio@gmail.com>
2025-08-19 15:37:54 -0400

    Primer Programa Hola Mundo

---
+++ Unidad I/HolaMundo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 65001");
+    system("cls");
+    cout << "¡Hola, Mundo!" << endl;
+    cout << "Este es un programa de ejemplo en C++." << endl;
+    cout << "Usando la versión del estándar C++ 17." << endl;
+    return 0;
+}
